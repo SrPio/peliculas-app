3dc0b2fc8631a3e6adbd73dde2c4cb948057d4bf
SrPio <andresfer1799@gmail.com>
2022-08-21 16:30:34 -0500

    Cambio visual (fondo de botones animado). Se agregó un fondo de video a cada botón de la sección de Viewers al hacer hover.

--- src/componets/Viewers/Viewers.tsx
+++ src/componets/Viewers/Viewers.tsx
@@ -1,16 +1,51 @@
-import React from 'react'
+import React from "react";
 import styles from "./Viewers.module.css";
 
 function Viewers() {
   return (
     <div className={styles.viewer_container}>
-      <div className={styles.option}><img src="/images/viewers-disney.png" alt="" /></div>
-        <div className={styles.option}><img src="/images/viewers-pixar.png" alt="" /></div>
-        <div className={styles.option}><img src="/images/viewers-marvel.png" alt="" /></div>
-        <div className={styles.option}><img src="/images/viewers-starwars.png" alt="" /></div>
-        <div className={styles.option}><img src="/images/viewers-national.png" alt="" /></div>
+      <div
+        className={`${styles.option} hover:scale-105 hover:border-white ease-in-out duration-300 drop-shadow-2xl`}
+      >
+        <video className={styles.videoTag} autoPlay loop muted>
+          <source src={"/images/DisneyVideo.mp4"} type="video/mp4" />
+        </video>
+        <img src="/images/viewers-disney.png" alt="" />
+      </div>
+      <div
+        className={`${styles.option} hover:scale-105 hover:border-white ease-in-out duration-300 drop-shadow-2xl`}
+      >
+        <video className={styles.videoTag} autoPlay loop muted>
+          <source src={"/images/DisneyPixarVideo.mp4"} type="video/mp4" />
+        </video>
+        <img src="/images/viewers-pixar.png" alt="" />
+      </div>
+      <div
+        className={`${styles.option} hover:scale-105 hover:border-white ease-in-out duration-300 drop-shadow-2xl`}
+      >
+        <video className={styles.videoTag} autoPlay loop muted>
+          <source src={"/images/DisneyMarvelVideo.mp4"} type="video/mp4" />
+        </video>
+        <img src="/images/viewers-marvel.png" alt="" />
+      </div>
+      <div
+        className={`${styles.option} hover:scale-105 hover:border-white ease-in-out duration-300 drop-shadow-2xl`}
+      >
+        <video className={styles.videoTag} autoPlay loop muted>
+          <source src={"/images/DisneyStarWarsVideo.mp4"} type="video/mp4" />
+        </video>
+        <img src="/images/viewers-starwars.png" alt="" />
+      </div>
+      <div
+        className={`${styles.option} hover:scale-105 hover:border-white ease-in-out duration-300 drop-shadow-2xl`}
+      >
+        <video className={styles.videoTag} autoPlay loop muted>
+          <source src={"/images/DisneyNatgVideo.mp4"} type="video/mp4" />
+        </video>
+        <img src="/images/viewers-national.png" alt="" />
+      </div>
     </div>
-  )
+  );
 }
 
-export default Viewers
+export default Viewers;
